1fe6aa7ede35849489468ce3a3345cc4d4cf4a56
Leonel Higuera <lhiguer1@asu.edu>
2023-03-12 11:22:53 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# File Metadata Microservice
+
+Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer')
+var upload = multer()
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/api/fileanalyse', upload.single('upfile'), async (req, res, next) => {
+  console.log(req.file)
+  res.json({
+    name: req.file?.originalname,
+    type: req.file?.mimetype,
+    size: req.file?.size,
+  })
+  next()
+})
+
+module.exports = router;
